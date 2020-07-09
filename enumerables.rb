4bd9bcbe3aa528dd7634007280916de4fd6c9109
Duran <danigduran@gmail.com>
2020-07-09 11:54:34 +0700

    add my_inject method

--- enumerables.rb
+++ enumerables.rb
@@ -127,142 +127,40 @@ module Enumerable
         end
         return arr
     end
+
+    def my_inject(arg = nil, sym = nil)
+      arr = self.to_a
+      if block_given?
+        if arg != nil
+          acc = arg
+          arr.my_each do |x|
+            acc = yield(acc,x)
+          end
+        else
+          acc = arr[0]
+          arr[1..arr.length-1].my_each do |x|
+            acc = yield(acc, x)
+          end
+        end
+      else
+        if arg != nil && sym != nil
+          acc = arg
+          arr.my_each do |x|
+            acc = acc.send(sym, x)
+          end
+        elsif arg.class == Symbol
+          acc = arr[0]
+          arr[1..arr.length-1].my_each do |x|
+            acc = acc.send(arg, x)
+          end
+        end 
+      end 
+      acc
+    end
     
 end
 
-puts 'my_each'
-
-[1, 2, 3, 4, 'hi'].my_each do |x|
-  puts x
-end
-p [2,4,7,11].my_each #<Enumerator: [2, 4, 7, 11]:my_each>
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_each_with_index'
-
-[1, 2, 3, 4, 'hi'].my_each_with_index { |value, index| puts "#{value} => #{index}" }
-[1, 2, 3, 4, 'hi'].my_each_with_index(5) { |value, index| puts "#{value} => #{index}" }
-puts [2,4,7,11].my_each_with_index  #<Enumerator: [2, 4, 7, 11]:my_each_with_index
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_select'
-
-result = [1, 2, 3, 4, 5, 6].select(&:even?) #=> [2, 4, 6]
-puts result
-puts "------------------------"
-block = proc { |num| num = 11 }
-puts [2,4,7,11].my_select(&block) #=> [11]
-puts "-------------------------"
-puts [2,4,7,11].my_select  #<Enumerator: [2, 4, 7, 11]:my_select>
-puts [1, 2].my_select { |num| num == 1 } #=> [1]
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_all?'
-
-puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-puts "1-------------------"
-puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-puts "-2------------------"
-puts %w[ant bear cat].my_all?(/t/) #=> false
-puts "--3-----------------"
-puts [1, 2i, 3.14].my_all?(Numeric) #=> true
-puts "---4----------------"
-puts [nil, true, 99].my_all? #=> false
-puts "----5---------------"
-puts [].my_all? #=> true
-puts "-----6--------------"
-puts [nil, false, true, []].my_all? #=> false
-puts "------7-------------"
-puts [1, 2.5, 'a', 9].my_all?(Integer) #=> false
-puts "-------8------------"
-puts %w[dog door rod blade].my_all?(/d/) #=> true
-puts "--------9-----------"
-puts [3,4,7,11].my_all?(3) #=> false
-puts "---------10----------"
-puts [1, false].my_all? #=> false
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_any??'
-puts "1-------------------"
-puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
-puts "-2------------------"
-puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
-puts "--3-----------------"
-puts %w[ant bear cat].my_any?(/d/) #=> false
-puts "---4----------------"
-puts [nil, true, 99].my_any?(Integer) #=> true
-puts "----5---------------"
-puts [nil, true, 99].my_any? #=> true
-puts "-----6--------------"
-puts [].my_any? #=> false
-puts "------7-------------"
-puts [nil, false, true, []].my_any? #=> true
-puts "-------8------------"
-puts [1, 2.5, 'a', 9].my_any?(Integer) #=> true
-puts "--------9-----------"
-puts %w[dog door rod blade].my_any?(/d/) #=> true
-puts "---------10----------"
-puts [3,4,7,11].my_any?(3) #=> true
-puts "-----------11--------"
-puts [1, false].my_any? #=> true
-puts "-------------12------"
-puts [1].my_any? #=> true
-puts "---------------13----"
-puts [nil].my_any? #=> false
-puts "-----------------14--"
-puts [nil, false, nil, false].my_any? #=> false
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_none?'
-
-puts %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
-puts %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
-puts %w[ant bear cat].my_none?(/d/) #=> true
-puts [1, 3.14, 42].my_none?(Float) #=> false
-puts [].my_none? #=> true
-puts [nil].my_none? #=> true
-puts [nil, false].my_none? #=> true
-puts [nil, false, true].my_none? #=> false
-puts [nil, false, true, []].my_none? #=> false
-puts [1, 2.5, 'a', 9].my_none?(Integer) #=> false
-puts %w[dog door rod blade].my_none?(/d/) #=> false
-puts [3,4,7,11].my_none?(3) #=> false
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_count'
-
-ary = [1, 2, 4, 2]
-puts ary.my_count #=> 4
-puts ary.my_count(2) #=> 2
-puts ary.my_count(&:even?) #=> 3
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_map'
-
-puts (1..4).my_map { |i| i * i } #=> [1, 4, 9, 16]
-puts (1..4).my_map { 'dog' } #=> ["dog", "dog", "dog", "dog"]
-puts %w[a b c].my_map(&:upcase) #=> ["A", "B", "C"]
-puts %w[a b c].my_map(&:class) #=> [String, String, String]
-puts [2,4,7,11].my_map #<Enumerator: [2, 4, 7, 11]:my_map
-my_proc = Proc.new {|num| num > 10 }
-puts [18, 22, 5, 6] .my_map(my_proc) {|num| num < 10 } # true true false false
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'my_inject'
-
-longest = %w[cat sheep bear].my_inject do |memo, word|
-  memo.length > word.length ? memo : word
+def multiply_els(arg)
+  arg.my_inject(:*)
 end
 
-puts longest #=> "sheep"
-
-puts (5..10).my_inject { |sum, n| sum + n } #=> 45
-puts (5..10).my_inject(2) { |sum, n| sum + n } #=> 47
-puts (1..5).my_inject(4) { |prod, n| prod * n } #=> 480
-puts [1, 1, 1].my_inject(:+) #=> 3
-puts [1, 1, 1].my_inject(2, :+) #=> 5
-
-puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
-puts 'multiply_els'
-
-puts multiply_els([2, 4, 5]) #=> 40
